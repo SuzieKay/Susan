c93bd36be1e49811d860d684a4e3e3e34d1f12cc
Susan <nakigandasus@gmail.com>
2015-02-23 08:42:18 -0800

    exerxise1

---
+++ functions.py
@@ -0,0 +1,22 @@
+#Number1
+def print_string(mystr1):
+    print mystr1
+
+print_string("perfect")
+
+
+#Number2
+def print_sring(mystr2):
+    print "Hello"+" "+ mystr2
+
+print_string("Ladies")
+
+#Number3
+num1=input("Please enter the first number:")
+num2=input("Please enter the second number:")
+num1 + num2
+
+#Number4
+num1=input("Please enter the first number:")
+num2=input("Please enter the second number:")
+num1 / num2
